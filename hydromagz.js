8dc3e019a0b28a8a30137abdb3ff37bc76f0f020
Ozik Jarwo <jarwoozik08@gmail.com>
2019-11-25 14:16:22 +0800

    Update hydromagz.js

--- hydromagz.js
+++ hydromagz.js
@@ -1,3 +1,5 @@
+//<![CDATA[
 var twLSN = ['jarwo-hydromagzz.blogspot.com'];
 var redirectURL = 'https://www.kodejarwo.com';
 function cekLSN(value,arr){ var status = false; for(var i=0; i<arr.length; i++){ var name = arr[i]; if(name == value){ status = true; break; } } return status; } var hst = window.location.hostname; if(cekLSN(hst, twLSN) == false) { window.location.href = redirectURL; }
+//]]>
